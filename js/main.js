4331e8f87475a32c4104634e1a73d9b2da4b8d3f
gr421_vidol <Danilcarol56@gmail.com>
2024-01-29 14:17:20 +0700

    Практическая работа №6

--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
 let app = new Vue({
     el: '#app',
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        reduceToCart(){
-            this.cart -=1
-        }
-    },
     data: {
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true,
+        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -26,11 +18,17 @@ let app = new Vue({
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        cart: 0,
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
         updateProduct(variantImage) {
             this.image = variantImage
         }
     }
 })
+
 
 
